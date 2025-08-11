bebc68af730cfdfccc59565424738cc6cb8a7ae7
John Naputi <jnaputi94.dev@gmail.com>
2025-08-10 18:14:16 -0600

    Added the initial testing library

---
+++ HandDandy.Tests/HandDandy_Tests.swift
@@ -0,0 +1,16 @@
+//
+//  HandDandy_Tests.swift
+//  HandDandy.Tests
+//
+//  Created by John Naputi on 8/10/25.
+//
+
+import Testing
+
+struct HandDandy_Tests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
